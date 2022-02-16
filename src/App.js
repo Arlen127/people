35aaa35a9b2a267899180831f556405885bc6751
Alex Som <alex.som86@gmail.com>
2022-02-16 14:33:32 +0600

    first lesson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Person from './components/Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React</h1>
+      {/* <img src="img/avatar.png" alt="" /> */}
+      <div className="wrapper">
+        <Person
+          name="Mike"
+          age="25"
+        />
+        <Person name="John" age="18" />
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Person/Person.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Person.css'
+import avatar from './avatar.png';
+
+function Person(props) {
+  return (
+    <div className="person">
+      <img className="img" src={avatar} alt="" />
+      <h3 className="title">{props.name}</h3>
+      <p className="age">Age: <span>{props.age}</span></p>
+    </div>
+  );
+}
+
+export default Person
