7dc88bd0a5a5ce0963a8675d962de15d565e2bd2
Arlen <arikylychbekov3@gmail.com>
2022-02-17 11:56:38 +0600

    all

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
+import {Component} from "react";
 import './App.css';
 import Person from './components/Person/Person';
 
-function App() {
+class App extends Component {
+  state ={
+    people:[
+      {name:'Mike', age:25,prof:"Programmer"},
+      {name:'John', age:35,prof:"Disaigner"},
+    ],
+  };
+
+  changeName = () =>{
+    const people = [...this.state.people];
+    const person = {...this.state.people[0]};
+    person.name = "Sam";
+    people[0] = person;
+
+    this.setState({people});
+  };
+  increaseAge = () =>{
+    const people = this.state.people.map(person =>{
+      return{
+        ...person,
+        age:person.age + 1,
+      };
+    })
+    this.setState({people});
+  }
+  render(){
   return (
     <div className="App">
       <h1>Hello React</h1>
+      <button onClick={this.changeName}>Change name</button>
+      <button onClick={this.increaseAge}>increase age</button>
       {/* <img src="img/avatar.png" alt="" /> */}
       <div className="wrapper">
-        <Person
-          name="Mike"
-          age="25"
-        />
-        <Person name="John" age="18" />
+        <Person 
+        name={this.state.people[0].name}
+        age={this.state.people[0].age}>
+        {this.state.people[0].prof}
+        </Person>
+        <Person 
+        name={this.state.people[1].name}
+        age={this.state.people[1].age}>
+          {this.state.people[1].prof}
+        </Person>
       </div>
 
     </div>
   );
+}
 }
 
 export default App;
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,6 +8,7 @@ function Person(props) {
       <img className="img" src={avatar} alt="" />
       <h3 className="title">{props.name}</h3>
       <p className="age">Age: <span>{props.age}</span></p>
+      <p>Professia: {props.children}</p>
     </div>
   );
 }
